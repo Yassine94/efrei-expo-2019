0cd4848c9c4633f23ccfdd8d2d31130460566450
Dylan DE SOUSA <20180206@B15-05.local>
2019-09-09 11:20:01 +0200

    Clean client

---
+++ client/src/screens/Home.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Screen = ({ navigation }) => {
+  return (
+    <></>
+  );
+}
+Screen.navigationOptions = {
+  title: 'Accueil'
+}
+export default Screen;
